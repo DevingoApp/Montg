a1850be828806c50b42e6db81fb39be34072cfdc
DevingoApp <samgsmag55@gmail.com>
2024-11-25 16:59:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const products = [
-  { id: 1, name: "منتج 1", price: "50$", image: "IMG-20241125-WA0002.jpg" },
+  { id: 1, name: "منتج 1", price: "10$", image: "IMG-20241125-WA0002.jpg" },
   { id: 2, name: "منتج 2", price: "75$", image: "fareda 2.jpg" },
   { id: 3, name: "منتج 3", price: "100$", image: "fareda 3.jpg" }
 ];
